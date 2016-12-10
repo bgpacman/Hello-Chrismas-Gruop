ef88f927d9b21c6ace5e90bf289ade6726c69e50
Lubcho Milushev <lubcho.milushev@gmail.com>
2016-12-11 01:36:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Chrismas_Gruop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Как да кажем здрасти
+
+            Console.WriteLine("Zdraveite, az sam Slavikus!");
+        }
+    }
+}
